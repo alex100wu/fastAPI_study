e8220d29bd0648413663f7b5d3241648622244d6
qq942948927 <wuchaoit@qq.com>
2026-03-19 09:36:49 +0800

    新增用户模块

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from enum import Enum
 from pydantic import BaseModel
+from user import router as user_router
 
 app = FastAPI()
+app.include_router(user_router)
+
 class ModelName(str, Enum):
     alexnet = "alexnet"
     resnet = "resnet"
