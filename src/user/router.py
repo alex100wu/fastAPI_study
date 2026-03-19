adf40c57350958de118919650ff1cb5d2997ab96
qq942948927 <wuchaoit@qq.com>
2026-03-19 17:03:36 +0800

    推送

--- src/user/router.py
+++ src/user/router.py
@@ -1,19 +1,26 @@
 from typing import Annotated
-
-from fastapi import APIRouter, Path, Query
-
+from pydantic import BaseModel, Field
+from fastapi import APIRouter, Path, Query,Cookie
 router = APIRouter()
 
+class User(BaseModel):
+    model_config = {"extra":"forbid"}
+    age:int = Field(le=100,gt=0)
+    name : str = Field('default_name',max_length=20)
+    tags : list[str] = []
 
 @router.get("/user")
 async def get_user():
     return ""
 
+@router.get("/user/setInfo")
+async def setInfo(userInfo:Annotated[User,Query()]):
+    return userInfo
 
 @router.get("/user/items/{item_id}")
 async def read_user_items(
     item_id: Annotated[int, Path(title="The ID of the item to get")],
-    q: Annotated[str | None, Query(alias="item-query")] = None,
+    q: Annotated[str | None, Query(alias="item-query",max_length=30)] = None,
 ):
     results = {"item_id": item_id}
     if q:
